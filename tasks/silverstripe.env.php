e0f41f3db6345898e8d7d1bf8f8fc39d1e3d67fa
Jeremy Shipman <jeremy@burnbright.net>
2017-09-11 21:05:31 +1200

    Fix silverstripe environment file generation task

--- tasks/silverstripe.env.php
+++ tasks/silverstripe.env.php
@@ -8,37 +8,38 @@ task('silverstripe:env', function () {
 
 	$output = '<?php';
 	
+	if($ss_environment_type = askChoice('Environment type:',
+		array('dev' => 'dev', 'test' => 'test', 'live' => 'live'), 'test'
+	)) {
+		$output .= "\ndefine(\"SS_ENVIRONMENT_TYPE\",\"${ss_environment_type}\");";
+	}
+
 	if($ss_database_server = ask('Database host:', 'localhost')){
 		$output .= "\ndefine(\"SS_DATABASE_SERVER\",\"${ss_database_server}\");";
 	}
 	if($ss_database_username = ask('Database username:')) {
-		$output .= "\ndefine(\"SS_DATABASE_USERNAME\",\"${ss_database_username}\")";
+		$output .= "\ndefine(\"SS_DATABASE_USERNAME\",\"${ss_database_username}\");";
 	}
 	if($ss_database_password = askHiddenResponse('Database password:')) {
-		$output .= "\ndefine(\"SS_DATABASE_PASSWORD\",\"${ss_database_password}\")";
+		$output .= "\ndefine(\"SS_DATABASE_PASSWORD\",\"${ss_database_password}\");";
 	}
 
 	if($ss_database_name = ask('Database name:')) {
-		$output .= "\ndefine(\"SS_DATABASE_NAME\",\"${ss_database_name}\")";
+		$output .= "\ndefine(\"SS_DATABASE_NAME\",\"${ss_database_name}\");";
 	}
 	if($ss_default_admin_username = ask('CMS default admin username:', 'cmsadmin')) {
-		$output .= "\ndefine(\"SS_DEFAULT_ADMIN_USERNAME\",\"${ss_default_admin_username}\")";
+		$output .= "\ndefine(\"SS_DEFAULT_ADMIN_USERNAME\",\"${ss_default_admin_username}\");";
 	}
 	if ($ss_default_admin_username) {
 		if($ss_default_admin_password = askHiddenResponse('CMS default admin password:')) {
-			$output .= "\ndefine(\"SS_DEFAULT_ADMIN_PASSWORD\",\"${ss_default_admin_password}\")";
+			$output .= "\ndefine(\"SS_DEFAULT_ADMIN_PASSWORD\",\"${ss_default_admin_password}\");";
 		}
 	}
-	if($ss_environment_type = askChoice('Environment type:',
-		array('dev' => 'dev', 'test' => 'test', 'live' => 'live'), 'test'
-	)) {
-		$output .= "\ndefine(\"SS_ERROR_LOG\",\"${ss_environment_type}\")";
-	}
 	if($ss_error_log = ask('Error log path:')) {
-		$output .= "\ndefine(\"SS_ERROR_LOG\",\"${ss_error_log}\")";
+		$output .= "\ndefine(\"SS_ERROR_LOG\",\"${ss_error_log}\");";
 	}
 	if($ss_error_email = ask('Error email:')) {
-		$output .= "\ndefine(\"SS_ERROR_EMAIL\",\"${ss_error_email}\")";
+		$output .= "\ndefine(\"SS_ERROR_EMAIL\",\"${ss_error_email}\");";
 	}
 	if($ss_use_basic_auth = askChoice('Use basic authentication (password protect site):',
 		array('yes' => 'yes', 'no' => 'no'), 'no'
